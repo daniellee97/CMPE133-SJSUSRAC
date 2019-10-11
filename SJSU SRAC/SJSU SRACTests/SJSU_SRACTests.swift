5f7aaedf135131897f767aa61e487a87f76b1f7b
squach28 <49498077+squach28@users.noreply.github.com>
2019-10-11 16:01:31 -0700

    Add files via upload

---
+++ SJSU SRAC/SJSU SRACTests/SJSU_SRACTests.swift
@@ -0,0 +1,36 @@
+//
+//  SJSU_SRACTests.swift
+//  SJSU SRACTests
+//
+//  Created by student on 10/11/19.
+//  Copyright © 2019 SJSU-Windows-Specialists. All rights reserved.
+//
+
+import XCTest
+@testable import SJSU_SRAC
+
+class SJSU_SRACTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
